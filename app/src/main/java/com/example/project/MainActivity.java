4598dda474448962bdd66b0b9d915b7aa1eb2fc4
Pontus Kronenberg <a21ponkr@student.his.se>
2022-05-25 09:27:55 +0200

    MainActivity.java
    Added string which grabs data from JSON_URL, and an onCreate with a JsonTask with a onClickListener on the aboutbutton. It also has a onPostExecute.
    
    activity_main.xml
    
    Accordingly I added the aboutbutton as well as recyclerview in activity_main.xml which comes from MainActivity.java

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,29 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    RecyclerView recyclerView;
+    private Button aboutButton;
+
+    private final String Tag = "==>";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a21ponkr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setAdapter(new AnimeAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        aboutButton = findViewById(R.id.aboutbutton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "clicked about btn");
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("==>", json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Anime>>() {}.getType();
+        List<Anime> listOfAnime = gson.fromJson(json, type);
+        Log.d("==>", "Amount of anime: "+listOfAnime.size());
+        Log.d("==>","Element 0 "+listOfAnime.get(0).toString());
+
     }
 
 }
