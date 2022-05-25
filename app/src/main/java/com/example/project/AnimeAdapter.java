6031c43f3eb93f3cca053d5f6ce0e21ab954dacb
Pontus Kronenberg <a21ponkr@student.his.se>
2022-05-25 11:03:25 +0200

    Added code in AnimeAdapter.java that extends the recyclerview.viewholder. this has a findViewById on the name which is itemView.
    AnimeAdapter.java has more code, here we has a String array with the anime and onCreateViewHolder, onBindViewHolder & getItemCount

--- app/src/main/java/com/example/project/AnimeAdapter.java
+++ app/src/main/java/com/example/project/AnimeAdapter.java
@@ -1,4 +1,33 @@
 package com.example.project;
 
-public class AnimeAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AnimeAdapter extends RecyclerView.Adapter<AnimeViewHolder>{
+    List<String> animeSeries = Arrays.asList("Welcome to the NHK!", "Tekkon Kinkreet","BECK: Mongolian Chop Squad","Steins;Gate","Sonny Boy");
+
+    @NonNull
+    @Override
+    public AnimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_anime, parent, false);
+        return new AnimeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AnimeViewHolder holder, int position){
+        holder.name.setText(animeSeries.get(position));
+    }
+
+    @Override
+    public int getItemCount(){
+        return animeSeries.size();
+    }
+
 }
--- app/src/main/java/com/example/project/AnimeViewHolder.java
+++ app/src/main/java/com/example/project/AnimeViewHolder.java
@@ -1,4 +1,15 @@
 package com.example.project;
 
-public class AnimeViewHolder {
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+
+public class AnimeViewHolder extends RecyclerView.ViewHolder {
+    public TextView name;
+
+    public AnimeViewHolder(@NonNull View itemView){
+        super(itemView);
+        name = itemView.findViewById(R.id.name);
+    }
 }
