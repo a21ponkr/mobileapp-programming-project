7cf15a8228342d6af3d7e245d8376d87fcc26ca1
Pontus Kronenberg <a21ponkr@student.his.se>
2022-06-03 23:13:01 +0200

    Anime class with id, name and company to get the id, name and company

--- app/src/main/java/com/example/project/Anime.java
+++ app/src/main/java/com/example/project/Anime.java
@@ -1,4 +1,24 @@
 package com.example.project;
 
 public class Anime {
+    private String id;
+    private String name;
+    private String company;
+
+    public Anime(String id, String name, String company) {
+        this.id = id;
+        this.name = name;
+        this.company = company;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCompany(){
+        return company;
+    }
+
 }
